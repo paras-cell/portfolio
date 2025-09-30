300a99fc3bb8f772dcf706201196bb385fbea61a
paras <paraskumar19025@gmail.com>
2025-09-30 13:58:12 +0530

    Removed unused files from root directory

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,8 @@ function App() {
       {showWelcome ? <Welcome onFinish={() => setShowWelcome(false)} /> 
        : ( 
          <div className="background">
-      <Nav />
+          <Welcome/>
+      {/* <Nav />
       <video 
         className="back-vid" autoPlay loop muted playsInline src="/galaxy.mp4">
       </video>
@@ -31,7 +32,7 @@ function App() {
       <section id="about" className="section"><About/></section>
       <section id="project" className="section"><Project/></section>
       <section id="tech" className="section"><Tech/></section>
-      <section id="contact" className="section"><Contact/></section> 
+      <section id="contact" className="section"><Contact/></section>  */}
     </div> 
       )} 
     </>
